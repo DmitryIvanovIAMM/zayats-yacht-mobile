f4b4588168ee56fe13a79a18adc197f2dee321cd
Dmytro Ivanov <dmitry.ivanov.iamm@gmail.com>
2025-09-26 08:01:33 +0300

    ZYM-10: revert background JS fix, use only CSS for white background (global.css)

--- app/login.tsx
+++ app/login.tsx
@@ -2,31 +2,55 @@ import ContactUs from "@/components/ContactUs/ContactUs";
 import LoginForm from "@/components/Login/LoginForm";
 import { ThemedView } from "@/components/ThemedView";
 import { useRef } from "react";
-import { StyleSheet, View } from "react-native";
+import { Platform, StyleSheet, View } from "react-native";
 
 export default function LoginScreen() {
   const fakeContactUsRef = useRef(null);
 
   return (
     <ThemedView style={styles.container}>
-      <View style={styles.formContainer}>
-        <LoginForm />
-      </View>
-      <View style={styles.fullWidth}>
-        <ContactUs ref={fakeContactUsRef} />
+      <View style={styles.restrictWodth}>
+        <View style={styles.formContainer}>
+          <LoginForm />
+        </View>
+        <View style={styles.fullWidth}>
+          <ContactUs ref={fakeContactUsRef} />
+        </View>
       </View>
     </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
+  scrollView: {
+    flex: 1,
+    backgroundColor: "#fff",
+    height: "100%",
+    minHeight: "100%",
+    justifyContent: "space-between"
+  },
+  restrictWodth: {
+    flex: 1,
+    maxWidth: 700,
+    width: "100%",
+    alignSelf: "center",
+    marginHorizontal: "auto",
+    justifyContent: "space-between"
+  },
   container: {
     flexDirection: "column",
     alignItems: "center",
-    backgroundColor: "white",
-    width: "100%",
+    backgroundColor: "#fff",
     minHeight: "100%",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
+    ...Platform.select({
+      web: {
+        width: "100%"
+      },
+      default: {
+        width: "100%"
+      }
+    })
   },
   formContainer: {
     width: "100%"
